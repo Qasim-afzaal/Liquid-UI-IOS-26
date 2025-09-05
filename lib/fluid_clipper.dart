41c9a875c30f7d491809261086404c677d02d122
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-09-05 05:32:59 +0500

    hide margins

--- lib/fluid_clipper.dart
+++ lib/fluid_clipper.dart
@@ -5,15 +5,11 @@ class FluidClipper extends CustomClipper<Path> {
   FluidEdge edge;
   double margin;
 
-  FluidClipper(this.edge, {this.margin = 0.0}) : super();
+  FluidClipper(this.edge, {this.margin = 0.0});
 
   @override
-  Path getClip(Size size) {
-    return edge.buildPath(size, margin: margin);
-  }
+  Path getClip(Size size) => edge.buildPath(size, margin: margin);
 
   @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return true;
-  }
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
